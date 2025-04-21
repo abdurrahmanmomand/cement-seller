58b9da0845d6e7bd295fe5182d1bb55bb42b1ebf
abdurrahmanmomand <abdurrahmanmomand931@gmail.com>
2025-04-21 13:07:45 +0500

    sixth

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,26 +3,26 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="{{ route('home') }}">
+        <a class="nav-link {{ request()->routeIs('home') ? '' : 'collapsed' }}" href="{{ route('home') }}">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('show-all.index') }}">
+        <a class="nav-link {{ request()->routeIs('show-all.index') ? '' : 'collapsed' }}" href="{{ route('show-all.index') }}">
           <i class="bi bi-table"></i>
           <span>Show All Record</span>
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('vendor') }}">
+        <a class="nav-link {{ request()->routeIs('vendor') ? '' : 'collapsed' }}" href="{{ route('vendor') }}">
           <i class="bi bi-table"></i>
           <span>Show Vendors Details</span>
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('customer') }}">
+        <a class="nav-link {{ request()->routeIs('customer') ? '' : 'collapsed' }}" href="{{ route('customer') }}">
           <i class="bi bi-table"></i>
           <span>Show Customer Details</span>
         </a>
